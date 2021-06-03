1ec06a1d7e7ae35a6ae7b946d712f9fefc2af528
mayureshw <mayureshw@github>
2021-06-03 20:37:42 +0530

    If one feed fails, log exception and continue

--- f2m.py
+++ f2m.py
@@ -3,6 +3,7 @@ from f2mrc import frc
 from feedstat import fs
 
 for feed in frc.feeds:
-    feed.run()
+    try: feed.run()
+    except Exception as e: print('','There was an error running this feed')
     if frc.dryrun: break
 if not frc.dryrun: fs.savestat()
